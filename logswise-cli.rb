e7f45513c0e5e17df6e03cdbe3a3b9fe7840cac1
k61b <kayrabtuncer@gmail.com>
2025-06-07 15:39:54 +0300

    chore: update version

--- logswise-cli.rb
+++ logswise-cli.rb
@@ -1,9 +1,9 @@
 class LogswiseCli < Formula
   desc "Logswise CLI is a command-line tool for note-taking, context-aware suggestions, and AI chat, powered by your local LLM (Ollama) and Supabase. It is designed for developers and teams who want to capture notes, get actionable suggestions, and chat with an assistant—all from the terminal."
   homepage "https://github.com/k61b/logswise-cli"
-  url "https://github.com/k61b/logswise-cli/releases/download/v0.0.4/logswise-cli"
-  sha256 "b022d11ca029b9dca5f37de9e6fa19e7956fa167b250dbfead5a105e83053961"
-  version "0.0.4"
+  url "https://github.com/k61b/logswise-cli/releases/download/v0.0.5/logswise-cli"
+  sha256 "997cc87f787951e4fc845bc3bb8c16079a6d4a25efe10f8600d4a89372a9321f"
+  version "0.0.5"
 
   def install
     bin.install "logswise-cli"
